c7b76c6c628ba2a77ee20aa951fb236fe1cdae96
Tom Clancy <tclancy@gmail.com>
2017-01-20 12:56:27 -0500

    Wire up remaining view

--- linkshortener/urls.py
+++ linkshortener/urls.py
@@ -6,7 +6,8 @@ from links import views
 
 urlpatterns = [
     url(r'^links/$', views.ShortenedLinkList.as_view()),
-    url(r'^links/(?P<pk>[0-9]+)/$', views.ShortenedLinkDetail.as_view()),
+    url(r'^links/(?P<pk>[0-9]{1, 20})/$', views.ShortenedLinkDetail.as_view()),
+    url(r'^links/(?P<shortened>[a-zA-Z0-9]{1, 10})/$', views.unshorten_url),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
