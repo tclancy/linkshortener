6c00b8a7f0c6f47f22ae9b4e1a85d599fe6a0d91
Tom Clancy <tclancy@gmail.com>
2017-01-20 15:39:59 -0500

    Starting to test API directly

--- links/tests.py
+++ links/tests.py
@@ -1,9 +1,10 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 from links.models import ShortenedLink
 
 
-class ShortenedLinkTests(TestCase):
+class ShortenedLinkTests(APITestCase):
     def setUp(self):
         self.shortened_link = ShortenedLink(url="http://example.org")
         self.shortened_link.save()
@@ -20,7 +21,11 @@ class ShortenedLinkTests(TestCase):
         self.assertEqual(1, ShortenedLink.objects.get(pk=self.shortened_link.id).hits)
 
     def test_unicode_urls(self):
-        pass
+        url = "https://twitter.com"
+        response = self.client.post("/links/", {"url": url}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(ShortenedLink.objects.count(), 1)
+        self.assertTrue(ShortenedLink.objects.filter(url=url).exists())
 
     def test_querystring_encodes_properly(self):
         pass
